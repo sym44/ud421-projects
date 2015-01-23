3017968ff4bf0b8bbbd88bfa1263fe5d7be4bf45
DimitarBabov <dimitarbabov@gmail.com>
2015-01-23 09:43:35 -0500

    1-23-14

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -23,14 +23,20 @@ print sys.path
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
+t0 = time()
 features_train, features_test, labels_train, labels_test = preprocess()
-
+print "preprocess time.",round(time()-t0,3),"s"
 
 #########################################################
 ### your code goes here ###
+
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_test, labels_test)
+print "training time.",round(time()-t0,3),"s"
+t0 = time()
 pred_test = clf.predict(features_test)
+print "prediction time.",round(time()-t0,3),"s"
 
 count = 0
 n_points=len(labels_test)
@@ -39,6 +45,7 @@ for i in range(0,n_points-1):
         count+=1
 
 print float(count)/float(n_points)
+
 print clf.score(features_test,labels_test)
 
 #########################################################
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,32 +1,50 @@
 #!/usr/bin/python
 
-""" 
+"""
     this is the code to accompany the Lesson 2 (SVM) mini-project
 
     use an SVM to identify emails from the Enron corpus by their authors
-    
+
     Sara has label 0
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
-sys.path.append("../tools/")
+from sklearn.svm import SVC
+sys.path.append("C:\\Users\\dgb_us\\ud421-projects\\tools")
 from email_preprocess import preprocess
-
+print sys.path
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
-
-
 #########################################################
 ### your code goes here ###
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+clf = SVC(C=10000.0,kernel="rbf", gamma=0.0)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time.",round(time()-t0,3),"s"
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time.",round(time()-t0,3),"s"
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+
 
 #########################################################
+print "Accuracy: ",acc
+print pred[10],":",pred[26],":",pred[50],":"
+print sum(pred)
+
+
 
 
